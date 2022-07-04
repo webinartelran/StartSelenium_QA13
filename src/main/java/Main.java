9d572c3c38b68978a1aec15cdc68d9b857e7491f
webinartelran <webinartelran@gmail.com>
2022-07-04 20:12:01 +0300

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        if(a == 5){
+            switch(a){
+                case 5:
+                    System.out.println(a);
+            }
+        }
+        
+    }
+
+}
