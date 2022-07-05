213bcb2ba29a79cfe7d6b353c2facff8dcb05575
webinartelran <webinartelran@gmail.com>
2022-07-05 11:01:44 +0300

    FirstTest

---
+++ src/test/java/StartTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartTest {
+
+    @BeforeMethod
+    public void preCondition(){
+
+    }
+
+    @Test
+    public void testName(){
+
+    }
+    
+    @Test
+    public void testPassword(){
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+
+    }
+}
