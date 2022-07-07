b83e152d1190b00c54075d32ed2917de3bbb3214
webinartelran <webinartelran@gmail.com>
2022-07-07 23:15:31 +0300

    Lesson_3

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -1,11 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class StartTest {
+    WebDriver wd;
 
     @BeforeMethod
     public void preCondition(){
+    wd = new ChromeDriver();
+    // wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+    wd
+            .navigate()
+            .to("https://contacts-app.tobbymarshall815.vercel.app/home");
+// chain
 
     }
 
