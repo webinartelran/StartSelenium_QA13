f18417be67ccd9b9e4e355740468f5efe95331ad
webinartelran <webinartelran@gmail.com>
2022-07-11 22:35:34 +0300

    Lesson_4

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartTest {
     WebDriver wd;
@@ -10,16 +14,48 @@ public class StartTest {
     @BeforeMethod
     public void preCondition(){
     wd = new ChromeDriver();
-    // wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+//    wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
     wd
             .navigate()
             .to("https://contacts-app.tobbymarshall815.vercel.app/home");
 // chain
+    // wd.navigate().forward()
+    // wd.navigate().back()
+    // wd.navigate().refresh()
 
     }
 
     @Test
     public void testName(){
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("div"));
+        System.out.println(elements.size());
+        // strategies
+        element = wd.findElement(By.linkText("ABOUT")); // old school
+        element = wd.findElement(By.partialLinkText("ABO")); // old school
+        element = wd.findElement(By.id("root"));
+        element = wd.findElement(By.className("container"));
+
+        // cssSelector
+            // by tagName
+        element = wd.findElement(By.cssSelector("div"));
+            // by className
+        element = wd.findElement(By.cssSelector(".container"));
+            // by ID
+        element = wd.findElement(By.cssSelector("#root"));
+            // by attribute
+        element = wd.findElement(By.cssSelector("[placeholder='Email']"));
+            // by attribute starts with
+        element = wd.findElement(By.cssSelector("[href^='Ho']"));
+            // by attribute ends with
+        element = wd.findElement(By.cssSelector("[href$='out']"));
+            // by attribute contains
+        element = wd.findElement(By.cssSelector("[href*='ogi']"));
+
+
+
+
+
 
     }
     
@@ -30,6 +66,9 @@ public class StartTest {
 
     @AfterMethod
     public void postCondition(){
+
+        // wd.close();
+        wd.quit();
 
     }
 }
